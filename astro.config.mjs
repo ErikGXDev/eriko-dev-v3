52f1f91f6d205c9a00eb6bb9dcaa1de1fee2a68d
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2023-06-16 22:19:24 +0200

    add site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://eriko.dev",
+  integrations: [tailwind(), sitemap(), preact()],
+});
