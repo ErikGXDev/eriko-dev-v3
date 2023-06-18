55b4be22b11311d49c7e4308dc45d8696404f18f
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2023-06-18 17:41:01 +0200

    maybe*4

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -9,7 +9,7 @@ export default function Pill({ color, children }: Props) {
   return (
     <div
       class={
-        "rounded-full p-1 px-2 md:p-2 md:px-4 " +
+        "rounded-full p-1 px-2 text-sm md:text-base md:p-2 md:px-4 " +
         ("bg-" + colors[color as keyof typeof colors] || "bg-amber-500")
       }
     >
