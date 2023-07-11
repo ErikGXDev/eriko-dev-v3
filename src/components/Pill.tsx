68a598aa63aaf28e7f99059ee6146b089fe2ca1c
ErikoX <54033728+ErikoXDev@users.noreply.github.com>
2023-07-11 22:40:17 +0200

    fixed website

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -12,7 +12,7 @@ export default function Pill({ color, children, inflate }: Props) {
       class={
         "rounded-full " +
         (inflate ? "text-sm p-2 px-2" : "text-sm p-1 px-2") +
-        "md:text-lg md:p-2 md:px-4 bg-light/5 " +
+        " md:text-lg md:p-2 md:px-4 bg-light/5 " +
         ("text-" + colors[color as keyof typeof colors] || "bg-amber-500")
       }
     >
